072cdfae41729ed66260116464caa491dd60d3ff
imtiaz <imtiazkp531@gmail.com>
2023-12-07 15:25:19 +0530

    confirm delete

--- src/Components/MyProjects.jsx
+++ src/Components/MyProjects.jsx
@@ -3,14 +3,37 @@ import { toast, ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import AddProject from './AddProject';
 import { deleteProjectAPI, userProjectAPI } from '../Services/allAPI';
-import Projects from '../Pages/Projects';
 import { addProjectResponseContext, editProjectResponsContext } from '../Contexts/ContextShare';
 import EditProject from './EditProject';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+// New DeleteConfirmationModal component
+function DeleteConfirmationModal({ show, handleClose, handleConfirmDelete }) {
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Confirm Delete</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Are you sure you want to delete this project?</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Cancel
+        </Button>
+        <Button variant="danger" onClick={handleConfirmDelete}>
+          Confirm Delete
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 
 function MyProjects() {
   const { addProjectRespon, setAddProjectResponse } = useContext(addProjectResponseContext);
   const { editProjectRespons, setEditProjectResponse } = useContext(editProjectResponsContext);
   const [userProjects, setUserProjects] = useState([]);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [projectToDelete, setProjectToDelete] = useState(null);
 
   const getUserProjects = async () => {
     if (sessionStorage.getItem("token")) {
@@ -28,19 +51,29 @@ function MyProjects() {
     }
   };
 
-  const handleDelete = async (id) => {
-    const token = sessionStorage.getItem("token");
-    const reqHeader = {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${token}`
-    };
-    const result = await deleteProjectAPI(id, reqHeader);
-    if (result.status === 200) {
-      // page reload
-      getUserProjects();
-    } else {
-      toast.error(result.response.data);
+  const handleDelete = (id) => {
+    setProjectToDelete(id);
+    setShowDeleteModal(true);
+  };
+
+  const handleConfirmDelete = async () => {
+    if (projectToDelete) {
+      const token = sessionStorage.getItem("token");
+      const reqHeader = {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      };
+      const result = await deleteProjectAPI(projectToDelete, reqHeader);
+      if (result.status === 200) {
+        // page reload
+        getUserProjects();
+        toast.success("Project deleted successfully");
+      } else {
+        toast.error(result.response.data);
+      }
     }
+    setShowDeleteModal(false);
+    setProjectToDelete(null);
   };
 
   useEffect(() => {
@@ -48,30 +81,38 @@ function MyProjects() {
   }, [addProjectRespon, editProjectRespons]);
 
   return (
-    <div className='card shadow p-2 mb-3'>
-      <div className='d-flex flex-column flex-md-row justify-content-between align-items-md-center'>
-        <h3 className='mb-3 mb-md-0'>My Projects</h3>
-        <div className='ms-md-auto'>
-          <AddProject />
+    <>
+      <div className='card shadow p-2 mb-3'>
+        <div className='d-flex flex-column flex-md-row justify-content-between align-items-md-center'>
+          <h3 className='mb-3 mb-md-0'>My Projects</h3>
+          <div className='ms-md-auto'>
+            <AddProject />
+          </div>
         </div>
+
+        <div className='mt-3'>
+          {userProjects?.length > 0 ? userProjects.map(Project => (
+            <div key={Project._id} className='border d-flex flex-column flex-md-row align-items-md-center rounded p-2 mt-3'>
+              <h5 className='mb-3 mb-md-0'>{Project?.title}</h5>
+              <div className='icon ms-md-auto d-flex mt-2'>
+                <EditProject Project={Project} />
+                <a href={`${Project.github}`} target='_blank' className='btn me-2 me-md-0'><i className="fa-brands fa-github fa-2x"></i></a>
+                <button onClick={() => handleDelete(Project._id)} className='btn'><i className="fa-solid fa-trash fa-2x"></i></button>
+              </div>
+            </div>
+          )) :
+            <p className='text-danger fw-bolder fs-5'>No Projects Uploaded yet</p>}
+        </div>
+        <ToastContainer position='top-right' autoClose={2000} theme='colored' />
       </div>
 
-      <div className='mt-3'>
-        {/* collection of user projects */}
-        {userProjects?.length > 0 ? userProjects.map(Project => (
-          <div key={Project._id} className='border d-flex flex-column flex-md-row align-items-md-center rounded p-2 mt-3'>
-            <h5 className='mb-3 mb-md-0'>{Project?.title}</h5>
-            <div className='icon ms-md-auto d-flex mt-2'>
-              <EditProject Project={Project} />
-              <a href={`${Project.github}`} target='_blank' className='btn me-2 me-md-0'><i className="fa-brands fa-github fa-2x"></i></a>
-              <button onClick={() => handleDelete(Project._id)} className='btn'><i className="fa-solid fa-trash fa-2x"></i></button>
-            </div>
-          </div>
-        )) :
-          <p className='text-danger fw-bolder fs-5'>No Projects Uploaded yet</p>}
-      </div>
-      <ToastContainer position='top-right' autoClose={2000} theme='colored' />
-    </div>
+      {/* Render the DeleteConfirmationModal component */}
+      <DeleteConfirmationModal
+        show={showDeleteModal}
+        handleClose={() => setShowDeleteModal(false)}
+        handleConfirmDelete={handleConfirmDelete}
+      />
+    </>
   );
 }
 
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -5,6 +5,7 @@ import ProjectCard from '../Components/ProjectCard';
 import { allProjectsAPI } from '../Services/allAPI';
 import { toast, ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
+import { Link } from 'react-router-dom';
 
 function Projects() {
   const [token, setToken] = useState("");
@@ -41,28 +42,31 @@ function Projects() {
     <>
       <Header />
 
+
       <div style={{ marginTop: '100px' }} className='projects mb-4'>
-        <h1 className='text-center mb-5'>All Projects</h1>
         <div className='container'>
           <Row className='justify-content-center'>
             <Col xs={12} md={8} lg={6}>
-              <div className='input-group'>
-                <span className='input-group-text'>
-                  <i className="fa-solid fa-magnifying-glass fa-rotate-90 text-muted" style={{ fontSize: '1.5rem' }}></i>
-                </span>
-                <input
-                  type="text"
-                  className='form-control'
-                  placeholder='Search Projects by Technologies used'
-                  onChange={e => setSearchKey(e.target.value)}
-                />
-              </div>
+             {token ?<div className='input-group'>
+               
+                  <span className='input-group-text'>
+                    <i className="fa-solid fa-magnifying-glass fa-rotate-90 text-muted" style={{ fontSize: '1.5rem' }}></i>
+                  </span>
+                  <input
+                    type="text"
+                    className='form-control'
+                    placeholder='Search Projects by Technologies used'
+                    onChange={e => setSearchKey(e.target.value)}
+                  />
+               
+              </div>: null}
             </Col>
           </Row>
         </div>
-
-        {token ?
+      {token ?
+           
           <Row className="mt-5 container-fluid p-4 gy-4">
+             <h1 className='text-center mb-5'>All Projects</h1>
             {allProjects?.length > 0 ? allProjects?.map(project => (
               <Col key={project.id} xs={12} md={6} lg={4}>
                 <ProjectCard project={project} />
@@ -73,8 +77,17 @@ function Projects() {
           </Row> :
           <div>
             <p style={{ fontSize: '30px' }} className='fw-bolder text-danger m-5 text-center'>Please Login To view all Projects!!!</p>
+            <div className='text-center'>
+                          <p>New User? Click here to <Link style={{textDecoration:'none',color:'green'}} to={'/register'}>Register</Link></p>
+                          <p>Already have an account? Click here to <Link style={{textDecoration:'none',color:'green'}} to={'/login'}>Login</Link></p>
+                        </div>
+
           </div>
         }
+       
+        
+
+       
       </div>
       <ToastContainer position='top-right' autoClose={2000} theme='colored' />
     </>
